3663152e8edc367b6a658e928da3cd21dce2bb0a
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-02-09 16:32:15 +0000

    Finished Lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
+const recipe1 = {
+  title: "Asian Glazed Chicken",
+  level: "Amateur Chef",
+  ingredients: [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs",
+  ],
+  cuisine: "Asian",
+  dishType: "main_course",
+  image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 40,
+  creator: "Chef LePapu",
+};
 
 //Method 1 : Using Async Await
 
@@ -19,6 +38,25 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+
+    await Recipe.create(recipe1);
+    console.log(recipe1.title);
+
+    await Recipe.insertMany(data);
+
+    data.forEach((element) => console.log(element.title));
+
+    await Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+
+    console.log("Duration was updated successfully!");
+
+    await Recipe.deleteOne({ title: "Carrot Cake" });
+    console.log("Carrot cake is no longer available");
+
+    mongoose.disconnect();
   } catch (error) {
     console.log(error);
   }
